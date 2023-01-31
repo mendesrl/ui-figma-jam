9635ac35b39a5a975e8d58bfe3ad719e4d15cf88
mendesrl <37834840+mendesrl@users.noreply.github.com>
2023-01-31 17:37:45 -0400

    2 files modified

--- src/components/nodes/Square.tsx
+++ src/components/nodes/Square.tsx
@@ -3,7 +3,12 @@ import { NodeProps, Handle, Position } from 'reactflow';
 export function Square(props: NodeProps) {
   return (
     <div className="bg-indigo-500 rounded w-[200px] h-[200px]">
-      <Handle id="right" type="source" position={Position.Right} />
+      <Handle
+        id="right"
+        type="source"
+        position={Position.Right}
+        className="-right-5 w-3 h-3 bg-red-700"
+      />
       <Handle id="left" type="source" position={Position.Left} />
     </div>
   );
