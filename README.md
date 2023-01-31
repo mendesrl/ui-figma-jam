b17ce09c26a7c2bdfecf719f9a63a25974df258e
mendesrl <37834840+mendesrl@users.noreply.github.com>
2023-01-31 11:21:50 -0400

    add readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ui-figma-jam
+FigJam is a free online whiteboard and collaboration tool that you and your team can use to brainstorm and organize ideas.
 
+In this repository you will find a FigJam inspiration code
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-flb9h8)
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import 'reactflow/dist/style.css';
 import ReactFlow, { Background, Controls } from 'reactflow';
+import { zinc } from 'tailwindcss/colors';
 
 function App() {
   return (
     <div className="w-screen h-screen">
       <ReactFlow>
-        <Background gap={12} size={2} color="#ccc" />
+        <Background gap={12} size={2} color={zinc[200]} />
+        <Controls />
       </ReactFlow>
     </div>
   );
