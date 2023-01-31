dfd180a64534e88be7bdf7ddc2484e586957aa0c
mendesrl <37834840+mendesrl@users.noreply.github.com>
2023-01-31 11:41:01 -0400

    2 files modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import 'reactflow/dist/style.css';
-import ReactFlow, { Background, Controls } from 'reactflow';
+import ReactFlow, { Background, Controls, Node } from 'reactflow';
 import { zinc } from 'tailwindcss/colors';
+import { Square } from './components/nodes/Square';
+
+const NODE_TYPES = {
+  square: Square,
+};
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 200,
+      y: 400,
+    },
+    data: {},
+  },
+];
 
 function App() {
   return (
     <div className="w-screen h-screen">
-      <ReactFlow>
+      <ReactFlow nodeTypes={NODE_TYPES} nodes={INITIAL_NODES}>
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
---
+++ src/components/nodes/Square.tsx
@@ -0,0 +1,5 @@
+import { NodeProps } from 'reactflow';
+
+export function Square(props: NodeProps) {
+  return <div className="bg-violet-588 rounded w-[200px] h-[200px]"> </div>;
+}
